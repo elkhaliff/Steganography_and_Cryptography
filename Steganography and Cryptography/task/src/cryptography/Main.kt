633df9121f8147bf87da7dc08eac01d06a06b101
elkhaliff <elkhaliff@gmail.com>
2022-11-15 23:24:48 +0500

    Initial commit

---
+++ Steganography and Cryptography/task/src/cryptography/Main.kt
@@ -0,0 +1,26 @@
+package cryptography
+
+private fun getString(message: String): String {
+    println(message)
+    return readln()
+}
+
+fun main() {
+    var command = getCommand()
+
+    while (command != "exit") {
+        runCommand(command)
+        command = getCommand()
+    }
+    println("Bye!")
+}
+
+private fun getCommand() = getString("Task (hide, show, exit):")
+
+private fun runCommand(command: String) {
+    when (command) {
+        "hide" -> "Hiding message in image."
+        "show" -> "Obtaining message from image."
+        else -> "Wrong task: $command"
+    }.let { println(it) }
+}
